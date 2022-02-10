fbabc05cccc1f60b1173f417e085f0388d19d2e0
Mucahit Bugday <mbugday@k2m11s01.42kocaeli.com.tr>
2022-02-10 12:45:22 +0300

    sont test

---
+++ 00_libft/libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbugday <mbugday@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 12:22:42 by mbugday           #+#    #+#             */
+/*   Updated: 2022/02/10 12:22:44 by mbugday          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = (void *)malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
+}
---
+++ 00_libft/libft/ft_strnstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbugday <mbugday@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/10 12:21:25 by mbugday           #+#    #+#             */
+/*   Updated: 2022/02/10 12:21:28 by mbugday          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	hayint;
+	size_t	needint;
+
+	hayint = 0;
+	if (*needle == 0)
+		return ((char *)haystack);
+	while (haystack[hayint] != 0 && hayint < len)
+	{
+		needint = 0;
+		while (haystack[hayint + needint] == needle[needint]
+			&& needle[needint] != 0 && needint + hayint < len)
+			needint++;
+		if (!needle[needint])
+			return ((char *)&haystack[hayint]);
+		hayint++;
+	}
+	return (NULL);
+}
